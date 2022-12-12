2d8527ab36b9356da973fdd10c3c3a48479dfd42
Vinicius Melo <vmelo.gon@gmail.com>
2022-12-12 01:23:45 -0300

    Implementa os métodos "authenticate", "user_exists", "logout", "login" e "create_user" em "Auth"; adiciona "session_id" e "last_accessed" na tabela "user".

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -20,13 +20,19 @@ class Auth extends BaseController
     $this->my_validator = new \App\Libraries\Validator($this->session);
   }
 
-  private function authenticate()
+  private function authenticate(): bool
   {
-    if (isset($_SESSION['user_id'])) return 1;
+    if (isset($_SESSION['user_id'])) {
+      $user_model = new User();
+      if ($user_model->find($_SESSION['user_id'])) {
+        $user = $userModel->find($_SESSION['user_id']);
+        if ($user['session_id'] == $this->session->session_id) return 1;
+      }
+    }
     return 0;
   }
 
-  private function user_exists(string $username)
+  private function user_exists(string $username): bool
   {
     $user_model = new User();
     if ($user_model->where('username', $username)->first() || $user_model->where('email', $username)->first()) return 1;
@@ -35,22 +41,37 @@ class Auth extends BaseController
 
   private function logout()
   {
+    $user_model = new User();
+    $user = $user_model->find($_SESSION['user_id']);
+    unset($user['session_id']);
+    $user_model->update($user['id'], $user);
     $this->session->destroy();
   }
 
-  private function login()
+  private function login(): bool
   {
-    if (user_exists($_POST['username'])) {
-
-      $_SESSION['user_id'] = $user_id;
+    $user_model = new User();
+    if ($this->user_exists($_POST['username'])) {
+      $user = $user_model->where('username', $username)->first() ?? $user_model->where('email', $username)->first();
+      if (!$user['password'] == $_POST['password']) return 0;
+      $user->set(['session_id' => $this->session->session_id])->update();
+      $_SESSION['user_id'] = $user['id'];
       return 1;
     }
-    $this->messenger->set_message('error', 'Não foi possível realizar o login. Por favor, verifique o nome de usuário e a senha.');
     return 0;
   }
 
   private function create_user()
   {
+    $data = [
+      'name' => $_POST['name'],
+      'email' => $_POST['email'],
+      'password' => $_POST['password']
+    ];
+    if (!empty($_POST['username'])) $data['username'] = $_POST['username'];
+    $user_model = new User();
+    if (!$user_model->insert($data, false)) return 0;
+    $_SESSION['user_id'] = $user_model->getInsertID();
     return 1;
   }
 
@@ -64,7 +85,7 @@ class Auth extends BaseController
       return view('auth/login', $data);
     } elseif ($method == 'post') {
       if (!$this->my_validator->validate_form('login')) return redirect()->back()->withInput();
-      $this->login();
+      if (!$this->login()) $this->messenger->set_message('error', 'Por favor, verifique o nome de usuário e a senha.');
       return redirect()->back();
     }
   }
--- user.sql
+++ user.sql
@@ -1,11 +1,13 @@
 CREATE TABLE `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `session_id` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_accessed` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
