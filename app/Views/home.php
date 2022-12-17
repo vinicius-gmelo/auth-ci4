79899e457ab8c942e8688fc7e2ae2b8f9624d218
Vinicius Melo <vmelo.gon@gmail.com>
2022-12-16 23:29:31 -0300

    Converte o nome do usuário com a função escape na página inicial.

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,7 +6,7 @@ $greeting = get_greeting();
 ?>
 <nav class="navbar navbar-expand-md" style="border-bottom: 1px black solid;">
   <div class="container-fluid">
-    <span><?= ucfirst($greeting) ?>, <?= $user['name'] ?></span>
+    <span><?= ucfirst($greeting) ?>, <?= esc($user['name'])) ?></span>
     <a class="nav-link" href="<?= base_url('/logout') ?>" style="text-decoration: underline;">Sair</a>
   </div>
 </nav>
