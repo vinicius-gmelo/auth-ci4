ea0855b2de97ae6b863d8ac455bb069ee9ff837e
Vinicius Melo <vmelo.gon@gmail.com>
2022-12-17 01:55:55 -0300

    Delete app/Views/auth directory

--- app/Views/auth/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo $this->extend('layouts/default');
-echo $this->section('content');
-include  __DIR__ . "/../partials/login_form.php";
-echo $this->endSection();
--- app/Views/auth/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo $this->extend('layouts/default');
-echo $this->section('content');
-include  __DIR__ . "/../partials/registration_form.php";
-echo $this->endSection();
